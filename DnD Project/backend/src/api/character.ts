d58f4cbb8a85eb86e847af285f9714b05ef25e5d
KyleKinyon <98056201+KyleKinyon@users.noreply.github.com>
2022-03-23 02:10:45 -0400

    Removed irrelevant comments

--- DnD Project/backend/src/api/character.ts
+++ DnD Project/backend/src/api/character.ts
@@ -67,12 +67,6 @@ router.post("/editCharacter", async (req, res) => {
 	}
 });
 
-// When a charId is provided, a single character will be pulled from the database
-// The commented code below is supposed to pull all characters tied to a given user
-// when charId = null. It DOES do just that, however, it crashes because for some 
-// reason it is trying to set jwt headers after they've already been set.
-// To test, uncomment the code and remove the the line directly below it.
-// Then, in Arc/Postman, set charId = null and watch backend for crashes
 router.get("/selectCharacter", async (req, res) => {
 	const { charId } = req.query;
 	const { _id: userId } = res.locals;
@@ -103,27 +97,6 @@ router.get("/selectCharacter", async (req, res) => {
 	return res.status(200).json({
 		data
 	});
-});
-
-// This is just a TEST route that I would like to remove once select is working properly
-router.post("/selectAllCharacters", async (req, res) => {
-	const { _id: userId } = res.locals;
-	if (userId === null || userId === undefined) {
-		return res.status(400).json({ error: "No user provided" });
-	}
-
-	let data = await Char.find({ userId: userId }).exec();
-	if (data) {
-		res.status(200).json({
-			data
-		})
-	} else {
-		data = await User.findOne({ _id: userId }).exec();
-		if (data) {
-			return res.status(400).json({ error: "User has no characters" })
-		}
-		return res.status(400).json({ error: "User does not exist" });
-	}
 });
 
 router.post("/deleteCharacter", async (req, res) => {
