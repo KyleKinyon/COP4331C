a39b18af1c446427eebd45c442fca239d8ce570f
asomwaru <asomwaru01@gmail.com>
2022-04-17 20:00:51 -0400

    Circles have a tooltip now

--- DnD Project/frontend/src/pages/Game.tsx
+++ DnD Project/frontend/src/pages/Game.tsx
@@ -10,6 +10,7 @@ interface Point {
   id: number;
   x: number;
   y: number;
+  letter: string;
 }
 
 let numPoints = 5;
@@ -20,11 +21,14 @@ export default function Game() {
 
   const updateData = useCallback(() => {
     let temp = [];
+    const letters = "abcdefghijklmnopqrstuvwxyz";
     for (let i = 0; i < numPoints; i++) {
+      const num = Math.floor(Math.random() * (25 - 0 + 1)) + 0;
       temp.push({
         id: i,
         x: Math.random() * 500,
         y: Math.random() * 500,
+        letter: letters[num],
       });
     }
 
@@ -38,40 +42,61 @@ export default function Game() {
   useEffect(() => {
     let zoom = d3.zoom().on("zoom", (e: any) => {
       // console.log(d3.zoomTransform());
-      d3.select("#main g").attr("transform", e.transform);
+      d3.select("#canvas g").attr("transform", e.transform);
     });
 
-    d3.select("#main").call(zoom as any);
-    d3.select("#main").on("click", (e) => {
+    d3.select("#canvas").call(zoom as any);
+    d3.select("#canvas").on("click", (e) => {
       const [x, y] = d3.pointer(e);
+
+      const num = Math.floor(Math.random() * (25 - 0 + 1)) + 0;
+      const letters = "abcdefghijklmnopqrstuvwxyz";
       setData([
         ...data,
         {
           id: data.length,
           x,
           y,
+          letter: letters[num],
         },
       ]);
     });
 
-    // d3.select("#main g").style("background-image", chosenMap.link);
+    d3.select("#main")
+      .append("div")
+      .attr("id", "tooltip")
+      .attr("style", "position: absolute; opacity: 0;");
 
-    d3.select("#main g")
+    d3.select("#canvas g")
       .selectAll("circle")
-      .on("mouseover", (e) => {
-        console.log(d3.pointer(e));
-      })
       .data(data)
       .join("circle")
       .attr("cx", (d) => d.x)
       .attr("cy", (d) => d.y)
-      .attr("r", 3);
+      .attr("r", 5)
+      .style("stroke", "rgba(180, 180, 180, 0.8)")
+      .on("mouseover", (_, i) => {
+        d3.select("#tooltip")
+          .transition()
+          .duration(200)
+          .style("opacity", 1)
+          .text(i.letter);
+      })
+      .on("mouseout", () => {
+        d3.select("#tooltip").style("opacity", 0);
+      })
+      .on("mousemove", (e) => {
+        d3.select("#tooltip")
+          .style("left", `${e.clientX + 5}px`)
+          .style("top", `${e.clientY + 5}px`);
+      });
   }, [data, chosenMap]);
 
   return (
     <Box width={1} height={1} overflow="hidden">
       <Navbar />
       <Box
+        position="relative"
         display="flex"
         flexDirection="column"
         sx={{
@@ -83,6 +108,7 @@ export default function Game() {
         <Grid container columns={4} height={1} width={1} overflow="hidden">
           <Grid item xs={3}>
             <Box
+              id="main"
               sx={{
                 height: 1,
                 display: "flex",
@@ -91,7 +117,7 @@ export default function Game() {
                 alignContent: "center",
               }}
             >
-              <svg id="main" width="100%" height="100%">
+              <svg id="canvas" width="100%" height="100%">
                 <g>
                   <image href={chosenMap.link} x="0" y="0" />
                 </g>
